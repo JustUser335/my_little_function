9258413825abc2e5e340b15cd933df5d1867d5eb
Stanislav <justuser335@gmail.com>
2021-12-28 12:50:56 +0300

    literal
    slideAnchorLinks

--- function.js
+++ function.js
@@ -1,11 +1,37 @@
+document.addEventListener('DOMContentLoaded',function()
+{
+    controller.transmitter();
+});
+var controller =
+{
+    transmitter: function()
+    {
+        this.slideAnchorLinks();
+        this.trimString();      
 
-function trimString(str,limitSimbol){
-    if(str.length > limitSimbol){
-        str = str.substr(0,limitSimbol);
-        str = `${str}...`;
+    },
+    slideAnchorLinks: function()
+    {
+        $("header").on("click","a", function (event) {
+            event.preventDefault();
+            var id  = $(this).attr('href'),
+                top = $(id).offset().top;
+            $('body,html').animate({scrollTop: top-70}, 1500);
+        });
+    },
+    
+    trimString: function (str,limitSimbol)
+    {
+        if(str.length > limitSimbol)
+        {
+            str = str.substr(0,limitSimbol);
+            str = `${str}...`;
+        }
+        return str;
     }
-    return str;
 }
+
+
 
 function depth(arr){// arr[1,0], arr.splice(0,1, arr) собрать
     if(arr.length >= 1){
